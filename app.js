60e0787771a260b8711e8ed7dee4fc2ecc3359a5
Luis Rujana <lmrujanass@gmail.com>
2020-12-15 23:25:58 -0500

    Added first two routes

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
-
 const PORT = process.env.PORT || 4001;
 
+//Sets up the Express app to handle data parsing.
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
+//Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) =>{
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 
 
